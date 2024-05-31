ebd9fb340b8fba58538db79178b4a7c3427e7423
Ploux <peter@humo.golf>
2024-05-31 14:04:23 -0500

    Initial Commit

---
+++ HorizontalWheelPicker/HorizontalWheelPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorizontalWheelPickerApp.swift
+//  HorizontalWheelPicker
+//
+//  Created by Peter Loux on 5/31/24.
+//
+
+import SwiftUI
+
+@main
+struct HorizontalWheelPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
